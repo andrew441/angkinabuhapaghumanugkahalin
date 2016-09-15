5b90870cbf61b37e20312716c0e116e1e5311830
michael andrew suarez <suarezandrew441@gmail.com>
2016-09-15 00:22:32 -0700

    Fix
    Fixing on broken heart

---
+++ wordpress/wp-content/themes/custom/classes/asdasd.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: C
+ * Date: 9/12/2016
+ * Time: 11:13 PM
+ */
+
+namespace nice;
+//use APP\WPDB_QUERIES;
+class test
+{
+    function __construct()
+    {
+        //
+        //$wpdb_queries = new WPDB_QUERIES();
+     }
+
+   /// public function testighrere() {}
+}
